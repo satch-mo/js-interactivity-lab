fa7207cff251b9a5f574a274be0df133cd0cbdbb
satch-mo <satchel0293@gmail.com>
2022-05-18 22:24:03 -0500

    intermediate lab SUCCESSgit status!

--- index.js
+++ index.js
@@ -23,17 +23,28 @@ const addMovie = (evt) => {
 
 const deleteMovie = (evt) => {
   evt.target.parentNode.remove(); // can't explain how it works, but delete button is real
-  message.textContent = "Movie deleted!"; // message pops up! see line 3
+  message.textContent = `Movie ${evt.target.parentNode.firstChild.textContent} deleted!`; // message pops up! see line 3 personalised as well. needed to use firstChild property to target specific sibling of parent element. in this html, there's form with siblings input(movie title) and button.
+  revealMessage(); // linking the function that removes the message
 };
 
 const crossOffMovie = (evt) => {
   evt.target.classList.toggle("checked"); //  calling in checked class! NOTE: DOES NOT NEED . for class -- this functionality works now too!
   if (evt.target.classList.contains("checked")) {
     // if true that this contains checked class...
-    message.textContent = "Movie watched!";
+    message.textContent = `${evt.target.textContent} watched!`; // personalises message with specific movie
   } else {
-    message.textContent = "Movie added back!";
+    message.textContent = `${evt.target.textContent} added back!`;
   } // EVERYTHING WORKS AS IT SHOULD. CORE LAB COMPLETE, STOKED!!!
+  revealMessage();
+};
+
+// intermediate beginning here
+const revealMessage = () => {
+  message.classList.remove("hide"); // fixed functionality adding and removing hide -- cool!
+  setTimeout(() => message.classList.add("hide"), 1000);
+  // setTimeout(() => message.remove(), 1000); // THIS IS HOW YOU REMOVE MSG AFTER 1 SEC. now link to functions that will use this (deleteMovie,crossOffMovie).. but was not the way instructions said to do
 };
 
 document.querySelector("form").addEventListener("submit", addMovie);
+
+// i'm so stoked. i was so frustrated today in class and during the lab. but not only did I finish the core lab, i figured out the intermediate section as well!! think this is fuzzy that will get clearer with application.
